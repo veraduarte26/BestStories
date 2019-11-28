ddf3b659a37ad499ab8aa1fbed65685f50a82131
Vera Lúcia Domingos Duarte <vera.algarve@gmail.com>
2019-11-28 15:52:24 +0000

    add Model

---
+++ BestStories/Model/Storie.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BestStories.Model
+{
+    public class Storie
+    {
+        public string title { get; set; }
+        public string uri { get; set; }
+        public string postedBy { get; set; }
+        public DateTime time { get; set; }
+        public int score { get; set; }
+        public int commentCount { get; set; }
+    }
+}
